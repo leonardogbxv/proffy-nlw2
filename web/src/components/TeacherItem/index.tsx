252bff8ae7da835ad816f1fdeb35504eedc9a2d9
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-08-05 17:30:08 -0300

    Add frontend features and connect with backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,33 +1,55 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://tommo.ricmais.com.br/wp-content/uploads/2020/07/Hideo-Kojima.jpg" alt="Hideo Kojima" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Hideo Kojima</strong>
-          <span>Design de Jogos</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>Hideo Kojima (小島 秀夫) é um designer japonês de jogos eletrônicos e diretor-executivo da  Kojima Productions.
-        <br /><br />
-        Constantemente reconhecido por fãs e especialistas da indústria de videogame como um dos mais influentes e inovadores diretores e roteiristas de jogos eletrônicos de todos os tempos. 
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 2.500,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
